1c59ab81c9471641bc44486302478795669c21ae
GASANTOS <gabriel.santos@capgemini.com>
2023-09-27 11:26:50 -0300

    Center Gabriel

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <main>
-          <div>
+          <div className="flex bg-slate-300  justify-center">
             <h1 className='font-bold'>Portifolio Gabriel</h1>
           </div>
       </main>
